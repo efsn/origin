c78871581913b050f5d5f2b8b4621035d5659b19
codeyn <asa_chan@163.com>
2015-01-26 22:50:31 +0800

    jsp smart upload(step one)

---
+++ src/main/java/org/jspsmart/upload/SmartUploadException.java
@@ -0,0 +1,9 @@
+package org.jspsmart.upload;
+
+public class SmartUploadException extends Exception{
+    private static final long serialVersionUID = -3012220439677127594L;
+
+    SmartUploadException(String msg){
+        super(msg);
+    }
+}
