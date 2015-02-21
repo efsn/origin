e3cd893cc3e84a9cb10e984bde3f2150e08a432b
codeyn <asa_chan@163.com>
2015-02-22 01:18:19 +0800

    Interceptor(pre,post,after)

---
+++ src/main/java/org/demo/interceptor/Interceptor1.java
@@ -0,0 +1,34 @@
+package org.demo.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+public class Interceptor1 extends HandlerInterceptorAdapter{
+    
+    @Override
+    public boolean preHandle(HttpServletRequest request,
+                             HttpServletResponse response,
+                             Object handler) throws Exception{
+        System.out.println("Pre-Handler-1");
+        return true;
+    }
+    
+    @Override
+    public void postHandle(HttpServletRequest request,
+                           HttpServletResponse response,
+                           Object handler,
+                           ModelAndView modelAndView) throws Exception{
+        System.out.println("Post-Handle-1");
+    }
+    
+    @Override
+    public void afterCompletion(HttpServletRequest request,
+                                HttpServletResponse response,
+                                Object handler,
+                                Exception ex)throws Exception{
+        System.out.println("After-Complition-1");
+    }
+}
