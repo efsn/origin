2425f4117818f18c3da572d3b9a1eea4e7c91f7b
codeyn <asa_chan@163.com>
2015-03-19 15:44:53 +0800

    Enhance RegExUtil

--- src/main/java/org/svip/util/RegExUtil.java
+++ src/main/java/org/svip/util/RegExUtil.java
@@ -19,6 +19,15 @@ public enum RegExUtil{
     
     
     
+    Integer("-?[1-9]\\d*|0"),
+    FLOAT("-?([1-9]\\d*|0)\\.\\d*"),
+    INLAND_PHONE("\\d{3}-\\d{8}|\\d{4}-\\d{7,8}"),
+    //begin from 10000
+    QQ("[1-9]\\d{4,}", true),
+    POST_CODE("[1-9]\\d{5}(?!\\d)"),
+    CARD_18("(\\d{6})(\\d{4})(\\d{2})(\\d{3})(\\d|X)"),
+    //2012-12-12
+    Y_M_D("(\\d{3}[1-9]|\\d{2}[1-9]\\d{1}|\\d{1}[1-9]\\d{2}|[1-9]\\d{3})-(((0[13578]|1[02])-(0[1-9]|[12]\\d|3[01]))|((0[469]|11)-(0[1-9]|[12]\\d|30))|(02-(0[1-9]|[1]\\d|2[0-8])))"),
     
     
     
@@ -27,6 +36,29 @@ public enum RegExUtil{
     
     
     
+    IP("((2[0-4]\\d|25[0-5]|[01]?\\d\\d?)\\.){3}(2[0-4]\\d|25[0-5]|[01]?\\d\\d?)"),
+    EMAIL("[\\w!#$%&'*+/=?^_`{|}~-]+(?:\\.[\\w!#$%&'*+/=?^_`{|}~-]+)*@(?:[\\w](?:[\\w-]*[\\w])?\\.)+[\\w](?:[\\w-]*[\\w])?"),
+//    TAG("(?<=<(\\w+)>).*(?=<///1>)"),
+    TAG("<(\\w+)>[^<]*</\\1>"),
+    URL("[a-zA-z]+://[^\\s]*"),
+    
+//    TAG("<(\\S*?)[^*]*>.*?|<.*?/>"),
+    
+    
+    //Chinese character
+    CHINESE("[\\u4e00-\\u9fa5]"),
+    DOUBLE_BYTES_CHAR("[^\\x00-\\xff]"),
+    
+    //special character such as ^%&',;=?$"
+    INVALID_CHAR("[^%&',;=?$\\x22]+"),
+    
+    
+    
+    
+    BLANK_LINE("\\n\\s*\\r"),
+    
+    //front last blank
+    FL_BLANK("\\s*|\\s*", true),
     
     
     
@@ -35,16 +67,18 @@ public enum RegExUtil{
     
     
     
+    //start with letter 5-16 allow letter, number or _
+    USERNAME("[a-zA-Z][a-zA-Z0-9_]{4,15}", true),
+    PASSWORD("[a-zA-Z][\\w{5,17}", true)
     
-    IP("((2[0-4]/d|25[0-5]|[01]?/d/d?)/.){3}(2[0-4]/d|25[0-5]|[01]?/d/d?)"),
-    EMAIL(""),
-    TAG_INNERHTML("(?<=<(/w+)>).*(?=<///1>)");
+    
+    ;
     
     private static final char ENTIRE_PRE = '^';
     private static final char ENTIRE_SUF = '$';
     
     private String regEx;
-    private boolean isEntire = true;
+    private boolean isEntire;
     
     private RegExUtil(String regEx, boolean isEntire){
         this(regEx);
---
+++ src/test/java/org/svip/util/RegExUtilTest.java
@@ -0,0 +1,27 @@
+package org.svip.util;
+
+public class RegExUtilTest{
+
+    public static void main(String[] args){
+        
+        System.out.println("Tag:"+RegExUtil.TAG.matches("<ehcache>sss</ehcache>"));
+        
+        System.out.println(RegExUtil.FL_BLANK.matches(" xx "));
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+    
+}
